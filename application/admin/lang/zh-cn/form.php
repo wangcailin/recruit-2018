4f7f3288b20e2ce1a0efdccd9cc6b403e324cea6
wangcailin <dreamcailin@hotmail.com>
2018-12-10 16:00:55 +0800

    debug

---
+++ application/admin/lang/zh-cn/form.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Name'  =>  '姓名',
+    'Major'  =>  '专业',
+    'School'  =>  '学校',
+    'Email'  =>  '邮箱',
+    'Tel'  =>  '手机号码',
+    'Graduate'  =>  '毕业年份',
+    'Position'  =>  '意向职位',
+    'Createtime'  =>  '创建时间'
+];
